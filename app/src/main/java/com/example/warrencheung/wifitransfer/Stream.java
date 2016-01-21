0cc4c1856cfc3adddba11a3bbb066ffa473792f8
Warren <warrencheung54@gmail.com>
2016-01-21 15:02:58 +0000

    yo

--- app/src/main/java/com/example/warrencheung/wifitransfer/Stream.java
+++ app/src/main/java/com/example/warrencheung/wifitransfer/Stream.java
@@ -1,10 +1,13 @@
 package com.example.warrencheung.wifitransfer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Stream extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

d2c13baca5e7a7feb63a8a2144458856586d0454
Warren Cheung <WarrenCheung@student-057.some.ox.ac.uk>
2016-01-21 12:22:23 +0000

    Initial commit

---
+++ app/src/main/java/com/example/warrencheung/wifitransfer/Stream.java
@@ -0,0 +1,13 @@
+package com.example.warrencheung.wifitransfer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stream extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream);
+    }
+}
